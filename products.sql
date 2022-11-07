457f3c5efe12ff89ea552fee8fb24142998bfd8f
yashaswinisadanala20 <117620736+yashaswinisadanala20@users.noreply.github.com>
2022-11-07 10:23:31 +0530

    Add files via upload

---
+++ products.sql
@@ -0,0 +1,4 @@
+create table products( price int, name varchar(100) primary key, peroid int ) 
+insert into Products values(2000,'cyccle',178); 
+insert into Products values(8000,'realme7',256); 
+insert into Products values(6000,'realme8',198); 
